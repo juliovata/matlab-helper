2336c48f5082868344433f903ceca0ded713090b
juliovata <jvata@umich.edu>
2024-05-01 23:57:14 -0400

    Changed location of launcher script to /home/jvata/bin

--- install.sh
+++ install.sh
@@ -20,32 +20,33 @@ else
   exit 1
 fi
 
-mkdir -p ${HOME}/matlab-ubuntu
+# TODO(juliovata): if /home/$USER/bin does not exist, create the directory
+mkdir -p ${HOME}/bin
 
 # TODO(juliovata) back up existing matlab.desktop file if it exists
 echo "Backing up current matlab.desktop file, if it exists..."
 
 # Remove previous version and setup folder
 echo "Removing previous version (if found)..."
-rm -f ${HOME}/matlab-ubuntu/matlab_launcher.sh
+rm -f ${HOME}/bin/matlab_launcher.sh
 rm -f ${HOME}/.local/share/applications/matlab.desktop
 
 echo "Downloading and Installing latest files..."
 
 # TODO(juliovata) for now just cp relevant files from current directory
-cp matlab_launcher.sh ${HOME}/matlab-ubuntu/
+cp matlab_launcher.sh ${HOME}/bin/
 cp matlab.desktop ${HOME}/.local/share/applications/
 
 # Add MATLAB executable to matlab_launcher.sh
-sed -i -e "s|\"MATLAB Executable Here With Full Path\"|${MATLAB_PATH}/MATLAB|g" ${HOME}/matlab-ubuntu/matlab_launcher.sh
+sed -i -e "s|\"MATLAB Executable Here With Full Path\"|${MATLAB_PATH}/MATLAB|g" ${HOME}/bin/matlab_launcher.sh
 
 # Add current version, icon path, and script executable path to desktop file
 sed -i -e "s|\"MATLAB Version Here\"|${MATLAB_VERSION}|g" ${HOME}/.local/share/applications/matlab.desktop
 
 # TODO add option for using system icon versus default icon?
 sed -i -e "s|\"Icon Path Here\"|${MATLAB_PATH}/cef_resources/matlab_icon.png|g" ${HOME}/.local/share/applications/matlab.desktop
-sed -i -e "s|\"Executable Path Here\"|${HOME}/matlab-ubuntu/matlab_launcher.sh|g" ${HOME}/.local/share/applications/matlab.desktop
+sed -i -e "s|\"Executable Path Here\"|${HOME}/bin/matlab_launcher.sh|g" ${HOME}/.local/share/applications/matlab.desktop
 
-chmod +x ${HOME}/matlab-ubuntu/matlab_launcher.sh
+chmod +x ${HOME}/bin/matlab_launcher.sh
 
 # TODO(juliovata) add file associations
